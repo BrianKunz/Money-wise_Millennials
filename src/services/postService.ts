08ce2f5d347b25465f64b640420ed001bb3e0898
randomstump <kunzie1122@gmail.com>
2023-05-26 23:02:42 -0400

    added services folder and files. need to actually update postServices, just laid out a quick layout prior to updating properly

---
+++ src/services/postService.ts
@@ -0,0 +1,119 @@
+import { IPost } from "../models/post.model";
+
+const baseURL = "http://localhost:3000/posts/";
+
+export const postService = {
+  getAll: async (post: IPost): Promise<IPost> => {
+    try {
+      const response = await fetch(`${baseURL}`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(post),
+      });
+
+      if (!response.ok) {
+        throw new Error("Failed to get all posts");
+      }
+
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error("Error getting all posts: ", error);
+      throw error;
+    }
+  },
+
+  getOne: async (post: IPost, id: string): Promise<IPost> => {
+    try {
+      const response = await fetch(`${baseURL}/${id}`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(post),
+      });
+
+      if (!response.ok) {
+        throw new Error("Failed to get all posts");
+      }
+
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error("Error getting all posts: ", error);
+      throw error;
+    }
+  },
+
+  create: async (post: IPost): Promise<IPost> => {
+    try {
+      console.log("Post data :", post);
+
+      const response = await fetch(`${baseURL}create`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(post),
+      });
+
+      if (!response.ok) {
+        throw new Error("Failed to create post");
+      }
+
+      const data = await response.json();
+      console.log(data);
+      return data;
+    } catch (error) {
+      console.error("Error creating post: ", error);
+      throw error;
+    }
+  },
+
+  update: async (post: IPost, id: string): Promise<IPost> => {
+    try {
+      console.log("Post data : ", post);
+
+      const response = await fetch(`${baseURL}/${id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(post),
+      });
+
+      if (!response.ok) {
+        throw new Error("Update failed");
+      }
+
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error("Error updating post: ", error);
+      throw error;
+    }
+  },
+
+  delete: async (post: IPost, id: string): Promise<IPost> => {
+    try {
+      const response = await fetch(`${baseURL}/${id}`, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(post),
+      });
+
+      if (!response.ok) {
+        throw new Error("Delete failed");
+      }
+
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error("Error deleting post: ", error);
+    }
+  },
+};
---
+++ src/services/userService.ts
@@ -0,0 +1,53 @@
+import { IUser } from "../models/user.model";
+
+const baseURL = "http://localhost:3000/users/";
+
+export const userService = {
+  create: async (user: IUser): Promise<IUser> => {
+    try {
+      console.log("User data: ", user);
+
+      const response = await fetch(`${baseURL}signup`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(user),
+      });
+
+      if (!response.ok) {
+        throw new Error("Failed to create user.");
+      }
+
+      const data = await response.json();
+
+      console.log(data);
+      return data;
+    } catch (error) {
+      console.error("Error signing up: ", error);
+      throw error;
+    }
+  },
+
+  login: async (user: IUser): Promise<IUser> => {
+    try {
+      const response = await fetch(`${baseURL}login`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(user),
+      });
+
+      if (!response.ok) {
+        throw new Error("Login failed");
+      }
+
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error("Error logging in: ", error);
+      throw error;
+    }
+  },
+};
