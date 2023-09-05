3b2d00187884a47623d4f538c641a1e788e8df4b
randomstump <kunzie1122@gmail.com>
2023-09-04 21:09:46 -0400

    added login/sign up styling

--- src/components/User/User.tsx
+++ src/components/User/User.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import CreateUser from "./CreateUser/createUser";
 import LoginUser from "./LoginUser/loginUser";
+import "./User.scss";
 
 export const User: React.FC = () => {
   const [isLogin, setIsLogin] = useState(true);
@@ -9,7 +10,7 @@ export const User: React.FC = () => {
   };
 
   return (
-    <div>
+    <div className="container">
       {isLogin ? <LoginUser /> : <CreateUser />}
       <button onClick={toggleForm}>
         {isLogin ? "Create an account" : "Login"}
